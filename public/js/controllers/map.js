c44da4f1e984168ac2bcad9a56ca28a87fdfa250
Teddy Otieno <teddy.asola@andela.com>
2017-02-15 22:53:45 +0300

    Initialize map, finish on navigation bar

--- public/js/controllers/map.js
+++ public/js/controllers/map.js
@@ -1,4 +1,6 @@
 angular.module('shnApp')
     .controller('MapCtrl', function($scope) {
-
+        window.onload = function() {
+            cartodb.createVis('map', 'https://saveharlemnow.carto.com/api/v2/viz/a6b9d08c-e9fc-11e6-a3b3-0e05a8b3e3d7/viz.json');
+        };
     });
