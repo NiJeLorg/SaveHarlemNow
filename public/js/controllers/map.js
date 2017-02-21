1ec6b3a2edee7b77194223edb7601d58ada91704
Teddy Otieno <teddy.asola@andela.com>
2017-02-21 16:07:18 +0300

    Initalize map with basemaplayer and center it around HTML

--- public/js/controllers/map.js
+++ public/js/controllers/map.js
@@ -1,6 +1,16 @@
 angular.module('shnApp')
     .controller('MapCtrl', function($scope) {
-        window.onload = function() {
-            cartodb.createVis('map', 'https://saveharlemnow.carto.com/api/v2/viz/a6b9d08c-e9fc-11e6-a3b3-0e05a8b3e3d7/viz.json');
-        };
+
+        function initMap() {
+            var map = L.map('map', {
+                center: [40.811550, -73.946477],
+                zoom: 13
+            });
+            var baseMapLayer = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+                maxZoom: 18,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
+            });
+            map.addLayer(baseMapLayer);
+        }
+        initMap();
     });
